f583043765775da67abd9774e964b0b3e958d564
Eric Umeohabike <32669797+eric2umeh@users.noreply.github.com>
2026-07-27 12:21:23 +0100

    Update for todo list router

--- week 3/react-demo/src/App.tsx
+++ week 3/react-demo/src/App.tsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.js"
-import About from "./pages/About.js"
-import Contact from "./pages/Contact.js"
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Contact from "./pages/Contact.js";
+import TodoList from "./pages/TodoList.js";
+import AddTodoForm from "./pages/AddTodoForm.js";
 
 export default function App() {
+  const [todos, setTodos] = useState<string[]>([]);
+
+  function addTodo(text: string) {
+    setTodos((currentTodos) => [...currentTodos, text.trim()]);
+  }
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<TodoList todos={todos} />} />
+      <Route path="/add" element={<AddTodoForm onAdd={addTodo} />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 }
-
