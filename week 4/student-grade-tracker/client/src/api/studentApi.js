f88ac75096400f60571958a480f71ca9a1b11981
Eric Umeohabike <32669797+eric2umeh@users.noreply.github.com>
2026-07-29 10:34:01 +0100

    Add student api

---
+++ week 4/student-grade-tracker/client/src/api/studentApi.js
@@ -0,0 +1,54 @@
+const API_URL = "http://localhost:3001";
+
+// Get All Students
+export async function getStudents() {
+  const response = await fetch(`${API_URL}/students`);
+
+  return response.json();
+}
+
+// Create Student
+export async function createStudent(student) {
+  const response = await fetch(`${API_URL}/students`, {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify(student),
+  });
+
+  return response.json();
+}
+
+// Update Student
+export async function updateStudent(id, student) {
+  const response = await fetch(`${API_URL}/students/${id}`, {
+    method: "PUT",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify(student),
+  });
+
+  return response.json();
+}
+
+// Delete Student
+export async function deleteStudent(id) {
+  const response = await fetch(`${API_URL}/students/${id}`, {
+    method: "DELETE",
+  });
+
+  return response.json();
+}
+
+// Get Summary
+export async function getSummary() {
+  const response = await fetch(`${API_URL}/summary`);
+
+  return response.json();
+}
