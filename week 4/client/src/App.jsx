9ddc450fed2d3f61b4665853f0d3580bf42c7ea2
Eric Umeohabike <32669797+eric2umeh@users.noreply.github.com>
2026-07-29 08:39:10 +0100

    App update

--- week 4/client/src/App.jsx
+++ week 4/client/src/App.jsx
@@ -3,19 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import AddTransaction from "./pages/AddTransaction";
 import TransactionsPage from "./pages/TransactionsPage";
 
-// Main Application
 function App() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={<AddTransaction />}
-      />
-
-      <Route
-        path="/transactions"
-        element={<TransactionsPage />}
-      />
+      <Route path="/" element={<AddTransaction />} />
+      <Route path="/transactions" element={<TransactionsPage />} />
     </Routes>
   );
 }
